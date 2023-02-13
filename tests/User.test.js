8f91fa70c17d4d0e6dc005d7dd5d5c753cd2af59
Muhammad <muhammadalnabhani@gmail.com>
2023-02-13 17:16:34 +0000

    created 3 test for userName,password and age

--- tests/User.test.js
+++ tests/User.test.js
@@ -3,7 +3,12 @@ describe("user integrity checks", function () {
   test(" User has a username", function () {
     const person1 = new User("Ducky", "hello", 18);
     expect(person1.userName).toBe("Ducky");
+  });
+  test("user has a password", function () {
+    const person1 = new User("Ducky", "hello", 18);
     expect(person1.password).toBe("hello");
+  });
+  test("user has an age", function () {
     expect(person1.age).toBe(18);
   });
 });
